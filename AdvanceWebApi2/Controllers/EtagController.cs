d8d7d13898b1f6209a3fe8ff4d992816333e8529
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 13:55:38 +0530

    Code Added

---
+++ AdvanceWebApi2/Controllers/EtagController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceWebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EtagController : ControllerBase
+    {
+
+        // code here toPerform HHTP caching.
+        //Create Etag middleware and configure middleware. Run multiple scenarios
+    }
+}
